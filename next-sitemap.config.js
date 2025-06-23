2ade92a2464f44d6afa20ed7cb209e04e59083ec
Gabriele d'Agostino <gabriele@MacBook-Pro-3.homenet.telecomitalia.it>
2025-06-23 22:59:50 +0200

    fix: aggiungi pagine manualmente alla sitemap per risolvere problema sitemap vuota

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,4 +4,33 @@ module.exports = {
   changefreq: 'weekly',
   priority: 0.7,
   sitemapSize: 5000,
+  additionalPaths: async (config) => {
+    const result = [
+      {
+        loc: '/',
+        changefreq: 'weekly',
+        priority: 1.0,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/discografia',
+        changefreq: 'weekly',
+        priority: 0.8,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/gabriele-dagostino',
+        changefreq: 'monthly',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/briele',
+        changefreq: 'weekly',
+        priority: 0.9,
+        lastmod: new Date().toISOString(),
+      },
+    ];
+    return result;
+  },
 }; 
